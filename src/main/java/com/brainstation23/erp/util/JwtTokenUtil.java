b75acdf64359ef6c168838caf21162f0c7a52a1c
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-04-17 15:49:36 +0600

    implement JwtTokenUtil class

---
+++ src/main/java/com/brainstation23/erp/util/JwtTokenUtil.java
@@ -0,0 +1,53 @@
+package com.brainstation23.erp.util;
+
+import java.util.Date;
+import java.util.function.Function;
+
+import com.brainstation23.erp.persistence.entity.UserEntity;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtTokenUtil {
+    private static final long EXPIRE_DURATION = 24 * 60 * 60 * 1000; // 24 hour
+
+    private String SECRET_KEY="secretKey";
+
+    public String generateAccessToken(UserEntity user) {
+        return Jwts.builder()
+                .setSubject(user.getId().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
+
+    }
+
+    public String extractUserId(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, UserEntity userDetails) {
+        final String userId = extractUserId(token);
+        return (userId.equals(userDetails.getId()) && !isTokenExpired(token));
+    }
+}
